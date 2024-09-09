a6f9ef44079472512b31a8eb2e58bcc00fbe9aa3
root <root@sy.>
2024-09-09 20:35:32 +0800

    fixed on java not recognizing mvn project

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
 	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvn" }, { upward = true })[1]),
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvn", "pom.xml" }, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
