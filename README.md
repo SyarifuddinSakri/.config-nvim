7a555c9cfba7a83950f18b5cfb74261f3a34b264
root <root@sy.>
2024-09-09 21:31:35 +0800

    fixed on the <> autopairing that occurs in Cpp. Make it only happen when coding in Java

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ This config is derived from starter nvim. I manually tweak the 'Tab' key to make
 
 1. When autosuggestion appears, pressing 'Tab' will automatically select the first selection.
 2. If cursor if before closing characters, Ex:('}','"',''',';',']') pressing tab will skip the character, positioning it after that closing character
+3. As i'm coding a lot with Java and using Maven build, this config is currently dedicated for that setup. But using other language should be fine, need only to install the language server for the used language.
--- nvim/lua/kickstart/plugins/autopairs.lua
+++ nvim/lua/kickstart/plugins/autopairs.lua
@@ -1,6 +1,5 @@
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
-
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
@@ -8,13 +7,28 @@ return {
 	dependencies = { "hrsh7th/nvim-cmp" },
 	config = function()
 		require("nvim-autopairs").setup({})
+
 		-- If you want to automatically add `(` after selecting a function or method
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		-- Add custom rule for angle brackets
+
+		-- Add custom rule for angle brackets only for Java files
 		local Rule = require("nvim-autopairs.rule")
 		local npairs = require("nvim-autopairs")
-		npairs.add_rule(Rule("<", ">")) -- Adding custom pair for angle brackets
+
+		-- Check if the current file type is Java
+		local add_java_angle_bracket_rule = function()
+			local filetype = vim.bo.filetype
+			if filetype == "java" then
+				npairs.add_rule(Rule("<", ">"))
+			end
+		end
+
+		-- Call the function on setup or file type change
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			callback = add_java_angle_bracket_rule,
+		})
 	end,
 }
