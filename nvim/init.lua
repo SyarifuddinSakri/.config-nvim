670414b6367f67b5183c059be2ce6e21baafeae5
root <root@SY.>
2024-09-26 13:09:59 +0800

    added Neotree

--- nvim/init.lua
+++ nvim/init.lua
@@ -467,6 +467,16 @@ require("lazy").setup({
 	{ "Bilal2453/luvit-meta", lazy = true },
 	{ "mfussenegger/nvim-jdtls", ft = { "java" } },
 	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{
 		-- Main LSP Configuration
 		"neovim/nvim-lspconfig",
 		dependencies = {
